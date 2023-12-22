d3137aac3d71d4c502c11de7292ddc74082e0c9b
HKumar6 <harishk9697@gmail.com>
2023-12-22 11:42:51 +0530

    Update test.sh file

--- test.sh
+++ test.sh
@@ -5,11 +5,13 @@
 #BRANCH_NAME="$2"
 #echo "Git Url: $GITHUB_URL"
 #echo "Git Branch name: $BRANCH_NAME"
-set -e
 
-folder_to_cleanup="/tests/playwright_repo"
+trap "aws s3 cp /tests/playwright_repo/playwright-report/index.html s3://tf-rf-scripts-spe-qaqc-bucket/PlaywrightReport/ && echo sile copied to s3 bucket"
 
-if [ -d "$folder_to_cleanup"]
+cd /tests
+folder_to_cleanup="playwright_repo"
+
+if [ -d "$folder_to_cleanup" ]
 then
     echo "cleaning up folder $folder_to_cleanup"
     rm -rf "$folder_to_cleanup"
@@ -17,37 +19,33 @@ else
     echo "$folder_to_cleanup does not exist."
 fi
 
-try() {
-    echo "before clone"
-    ## Clone the Github repository
-    git clone --single-branch --branch main https://github.com/Harishk9697/playwright-test-suite.git /tests/playwright_repo
-    
-    echo "change directory to playwright repo"
-    cd /tests/playwright_repo
+echo "before clone"
+## Clone the Github repository
+git clone --single-branch --branch main https://github.com/Harishk9697/playwright-test-suite.git /tests/playwright_repo
+if [ $? -ne 0 ]; then
+    echo "Command Git clone failed"
+fi
 
-    echo "list the files"
-    ls
-    
-    echo "Install npm"
-    ## Install dependencies
-    npm install
-    
-    echo "Run playwright testcases"
-    ## RUN tests
-    npx playwright test
-}
+echo "change directory to playwright repo"
+cd /tests/playwright_repo
+echo "list the files"
+ls
 
-catch() {
-    echo "An error occured:"
-    #echo "$BASH_COMMAND"
-    #echo "$@"
-}
+echo "Install npm"
+## Install dependencies
+npm install
+if [ $? -ne 0 ]; then
+    echo "Command npm install failed"
+fi
 
-finally() {
-    ## Copy generated report to s3 bucket
-    aws s3 cp /tests/playwright_repo/playwright-report/index.html s3://tf-rf-scripts-spe-qaqc-bucket/PlaywrightReport/
-}
+echo "Run playwright testcases"
+## RUN tests
+npx playwright test
+if [ $? -ne 0 ]; then
+    echo "Command npx playwright test failed"
+fi
 
-try
-catch
-finally
+#finally() {
+#    ## Copy generated report to s3 bucket
+#    aws s3 cp /tests/playwright_repo/playwright-report/index.html s3://tf-rf-scripts-spe-qaqc-bucket/PlaywrightReport/
+#}
